7efe8478049abaa688d724160cc50af4c0e9abee
kinasis <kinasis3000@gmail.com>
2021-03-09 11:49:09 +0900

    @next/bundle-analyzerを追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const defaultConfig = {
     reactStrictMode: true,
     env: {
         customKey: 'test',
     }
 };
+
+// https://www.npmjs.com/package/@next/bundle-analyzer
+// ANALYZEという環境変数にtrueが入っているときにWebpack Bundle Analyzerが実行される。
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(defaultConfig);
