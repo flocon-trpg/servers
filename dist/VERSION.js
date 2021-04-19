82753f27e704b39fdd57419cc44f15363a443c89
kinasis <kinasis3000@gmail.com>
2021-04-19 19:54:10 +0900

    update npm package

--- dist/VERSION.js
+++ dist/VERSION.js
@@ -7,7 +7,7 @@ const VERSION = new semver_1.SemVer({
     patch: 0,
     prerelease: {
         type: semver_1.alpha,
-        version: 5,
+        version: 6,
     }
 });
 exports.default = VERSION;
