6b5d31403f7dab0630fe85177407ef1f4563aef7
kinasis <kinasis3000@gmail.com>
2021-05-03 09:51:21 +0900

    Update 404 and 500

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+    // ダークテーマだとデフォルトの404ページは見えにくくなるので自前で作成している。
+    return <h1>404 - Not Found</h1>;
+};
+
+export default NotFound;
