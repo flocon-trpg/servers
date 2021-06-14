77286e3ef2bad370113cade1a8f3fbc2dfddf6c9
kizahasi <rasis.aneki@gmail.com>
2021-06-14 21:18:38 +0900

    release v0.3-alpha.1

--- dist/VERSION.js
+++ dist/VERSION.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const util_1 = require("@kizahasi/util");
 const VERSION = new util_1.SemVer({
     major: 0,
-    minor: 2,
+    minor: 3,
     patch: 0,
     prerelease: {
         type: util_1.alpha,
-        version: 17,
+        version: 1,
     }
 });
 exports.default = VERSION;
--- src/VERSION.ts
+++ src/VERSION.ts
@@ -2,11 +2,11 @@ import { SemVer, alpha } from '@kizahasi/util';
 
 const VERSION = new SemVer({
     major: 0,
-    minor: 2,
+    minor: 3,
     patch: 0,
     prerelease: {
         type: alpha,
-        version: 17,
+        version: 1,
     }
 });
 
