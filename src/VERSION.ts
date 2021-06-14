7095a2c97f14bf46b78e86dc8d707797009663a7
kizahasi <rasis.aneki@gmail.com>
2021-06-14 21:18:43 +0900

    release v0.3-alpha.1

--- src/VERSION.ts
+++ src/VERSION.ts
@@ -2,11 +2,11 @@ import { alpha, SemVer } from '@kizahasi/util';
 
 const VERSION = new SemVer({
     major: 0,
-    minor: 2,
+    minor: 3,
     patch: 0,
     prerelease: {
         type: alpha,
-        version: 25,
+        version: 1,
     }
 });
 
