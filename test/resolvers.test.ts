809e2886eafd93cc7f05d1e5c6e34d9c518d960d
kizahasi <rasis.aneki@gmail.com>
2021-08-29 01:28:56 +0900

    fix type-check error

--- test/resolvers.test.ts
+++ test/resolvers.test.ts
@@ -1,8 +1,6 @@
 import * as $MikroORM from '../src/graphql+mikro-orm/entities/room/mikro-orm';
 import { EM } from '../src/utils/types';
 import { User as User$MikroORM } from '../src/graphql+mikro-orm/entities/user/mikro-orm';
-import { UpOperation } from '@kizahasi/flocon-core';
-import { BaasType } from '../src/enums/BaasType';
 import { createApolloClient } from './createApolloClient';
 import { createTestServer } from './createTestServer';
 import { Resources } from './resources';
@@ -18,10 +16,6 @@ import { EntryToServerResultType } from '../src/enums/EntryToServerResultType';
 
 const timeout = 20000;
 
-const clientId = (i?: number) => `CLIENT_ID${i ?? ''}`;
-
-type RoomValueOperation = Omit<UpOperation, 'bgms' | 'paramNames' | 'participants'>;
-
 const resetDatabase = async (em: EM): Promise<void> => {
     for (const room of await em.find($MikroORM.Room, {})) {
         await $MikroORM.deleteRoom(em, room);
@@ -30,85 +24,6 @@ const resetDatabase = async (em: EM): Promise<void> => {
         em.remove(user);
     }
     await em.flush();
-};
-
-const setupRoomAndUsersAndParticipants = ({
-    em,
-    setupRoom,
-}: {
-    em: EM;
-    setupRoom?: (room: $MikroORM.Room) => void;
-}) => {
-    const roomName = 'ROOM_NAME';
-    const creatorUserUid = 'CREATOR_ID';
-    const creatorName = 'CREATOR_NAME';
-    // nonCreatorは、creatorではないがoperateすることがあるユーザー。
-    // anotherUserは、creatorではないしoperateにも関わらないユーザー。nonCreatorと区別するほうがコードがわかりやすくなるのでわけている。
-    // これらは名前がわかりにくいのでリネームするほうがよさそうか。
-    const nonCreatorUserUid = 'NON_CREATOR_ID';
-    const nonCreatorName = 'NON_CREATOR_NAME';
-    const anotherUserUid = 'ANOTHER_USER_ID';
-    const anotherName = 'ANOTHER_USER_NAME';
-
-    const room = new $MikroORM.Room({
-        name: roomName,
-        createdBy: creatorUserUid,
-        value: {
-            $version: 1,
-
-            boolParamNames: {},
-            numParamNames: {},
-            strParamNames: {},
-            boards: {},
-            characters: {},
-            bgms: {},
-            memos: {},
-            participants: {
-                [creatorUserUid]: {
-                    $version: 1,
-
-                    // 現状はとりあえず全員がMasterのケースのみを考えている。
-                    role: 'Master',
-                    name: creatorName,
-                    imagePieceValues: {},
-                },
-            },
-            activeBoardKey: null,
-            publicChannel1Name: 'メイン',
-            publicChannel2Name: 'メイン2',
-            publicChannel3Name: 'メイン3',
-            publicChannel4Name: 'メイン4',
-            publicChannel5Name: 'メイン5',
-            publicChannel6Name: 'メイン6',
-            publicChannel7Name: 'メイン7',
-            publicChannel8Name: 'メイン8',
-            publicChannel9Name: 'メイン9',
-            publicChannel10Name: 'メイン10',
-        },
-    });
-    if (setupRoom != null) {
-        setupRoom(room);
-    }
-
-    const creatorUser = new User$MikroORM({ userUid: creatorUserUid, baasType: BaasType.Firebase });
-    creatorUser.isEntry = true;
-    em.persist([room]);
-
-    return {
-        room,
-        creator: {
-            userUid: creatorUserUid,
-            name: creatorName,
-        },
-        nonCreator: {
-            userUid: nonCreatorUserUid,
-            name: nonCreatorName,
-        },
-        anotherUser: {
-            userUid: anotherUserUid,
-            name: anotherName,
-        },
-    };
 };
 
 it.each(['SQLite', 'PostgreSQL'] as const)(
