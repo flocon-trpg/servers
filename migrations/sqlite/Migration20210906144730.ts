407a1d750f911b6b2c7d75095ed186b35145b7b8
kizahasi <rasis.aneki@gmail.com>
2021-09-07 00:29:34 +0900

    fix SQLite migration code

--- migrations/sqlite/Migration20210906144730.ts
+++ migrations/sqlite/Migration20210906144730.ts
@@ -4,7 +4,5 @@ export class Migration20210906144730 extends Migration {
     async up(): Promise<void> {
         this.addSql('alter table `room` add column `created_at` datetime null;');
         this.addSql('create index `room_created_at_index` on `room` (`created_at`);');
-
-        this.addSql('create index `room_created_at_index` on `room` (`created_at`);');
     }
 }
