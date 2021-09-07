a6e38843629c1a2e940721f7353238b2632d6874
kizahasi <rasis.aneki@gmail.com>
2021-09-07 22:57:16 +0900

    add 'Uploader' type to filePath

--- src/internal/ot/filePath/v1.ts
+++ src/internal/ot/filePath/v1.ts
@@ -1,9 +1,14 @@
 import * as t from 'io-ts';
 
 export const Default = 'Default';
+export const Uploader = 'Uploader';
 export const FirebaseStorage = 'FirebaseStorage';
 
-export const sourceType = t.union([t.literal(Default), t.literal(FirebaseStorage)]);
+export const sourceType = t.union([
+    t.literal(Default),
+    t.literal(Uploader),
+    t.literal(FirebaseStorage),
+]);
 
 export const filePath = t.type({
     $v: t.literal(1),
