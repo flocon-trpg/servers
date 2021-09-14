e259ffafb6db086f1a893907454489e5bbd9af78
kizahasi <rasis.aneki@gmail.com>
2021-09-14 20:28:10 +0900

    デバッグしやすいように、一時的にuseIdTokenにconsole.logを追加

--- src/hooks/useIdToken.ts
+++ src/hooks/useIdToken.ts
@@ -5,11 +5,15 @@ export const useIdToken = () => {
     const user = useFirebaseUser();
     const [result, setResult] = React.useState<string>();
     React.useEffect(() => {
+        console.log('user is updated: %o', user);
         if (typeof user === 'string') {
             setResult(undefined);
             return;
         }
-        user.getIdToken().then(uid => setResult(uid));
+        user.getIdToken().then(idToken => {
+            console.log('idToken is updated');
+            setResult(idToken);
+        });
     }, [user]);
     return result;
 };
