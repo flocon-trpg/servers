a9a284648f07bb83d682995e575a61f2ca204371
kizahasi <rasis.aneki@gmail.com>
2021-09-27 00:15:14 +0900

    test: add if test without else

--- test/index.test.ts
+++ test/index.test.ts
@@ -141,6 +141,21 @@ a;
 });
 
 describe('if', () => {
+    test.each([true, false])('if', bool => {
+        const actual = exec(
+            `
+let result = 0;
+let x = ${bool ? 'true' : 'false'}
+if (x) {
+    result = 1;
+}
+result;
+        `,
+            {}
+        );
+        expect(actual.result).toBe(bool ? 1 : 0);
+    });
+
     test.each([true, false])('if-else', bool => {
         const actual = exec(
             `
