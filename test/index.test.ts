0fc704cbc4b252f5aa6e29450d5a0fbc12cb9aeb
kizahasi <rasis.aneki@gmail.com>
2021-09-27 00:59:31 +0900

    test: add switch tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -174,6 +174,59 @@ result;
     });
 });
 
+describe('switch', () => {
+    test.each([0, 1, 2, 3])('with default', i => {
+        const actual = exec(
+            `
+let result = -1;
+let i = ${i};
+switch (i) {
+    case 0:
+        result = '0';
+        break;
+    case 1:
+    case 2:
+        result = '1or2';
+        break;
+    default:
+        result = 'default';
+        break;
+}
+result;
+        `,
+            {}
+        );
+        let expected = 'default';
+        switch (i) {
+            case 0:
+                expected = '0';
+                break;
+            case 1:
+            case 2:
+                expected = '1or2';
+                break;
+        }
+        expect(actual.result).toBe(expected);
+    });
+
+    test.each([0, 1])('without default', i => {
+        const actual = exec(
+            `
+let result = 'default';
+let i = ${i};
+switch (i) {
+    case 0:
+        result = '0';
+        break;
+}
+result;
+        `,
+            {}
+        );
+        expect(actual.result).toBe(i === 0 ? '0' : 'default');
+    });
+});
+
 describe('Array', () => {
     test('isArray', () => {
         const actual = exec(
