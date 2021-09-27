8ed9002cef70ee0a5650f794d105f402fe76a73f
kizahasi <rasis.aneki@gmail.com>
2021-09-27 16:44:49 +0900

    fix: isValidKeyの文字数を10以下に制限

--- src/internal/ot/util/isValidKey.ts
+++ src/internal/ot/util/isValidKey.ts
@@ -1,4 +1,7 @@
 // (不正な|悪意のある)キーが混入するおそれがあるのはserverTransformのときのみなので、serverTransform以外では使わなくてよい
 export const isValidKey = (key: string): boolean => {
+    if (key.length >= 11) {
+        return false;
+    }
     return key.match(/^([0-9a-zA-Z]|-|_)+$/g) != null;
 };
