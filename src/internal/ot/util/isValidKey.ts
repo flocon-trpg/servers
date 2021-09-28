22cb07a30a7128a75e5b454c346bc3981889d346
kizahasi <rasis.aneki@gmail.com>
2021-09-28 23:48:44 +0900

    fix: isValidKeyの文字数制限が厳しすぎたため、Firebase Authenticationのuidを使用できなかった問題を修正

--- src/internal/ot/util/isValidKey.ts
+++ src/internal/ot/util/isValidKey.ts
@@ -1,6 +1,7 @@
 // (不正な|悪意のある)キーが混入するおそれがあるのはserverTransformのときのみなので、serverTransform以外では使わなくてよい
 export const isValidKey = (key: string): boolean => {
-    if (key.length >= 11) {
+    // Firebase Authenticationのuidは28文字のようなので、最低でもその文字数は許容しなければならない
+    if (key.length >= 40) {
         return false;
     }
     return key.match(/^([0-9a-zA-Z]|-|_)+$/g) != null;
