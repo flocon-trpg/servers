f4bdcdf09dfc0ffe74abdf5ecde4ba84efe888f9
kizahasi <rasis.aneki@gmail.com>
2021-10-03 00:41:18 +0900

    feat: メールアドレス以外の認証にも対応

--- src/configType.ts
+++ src/configType.ts
@@ -10,18 +10,32 @@ const api = t.type({
     url: maybe(url),
 });
 
+const provider = t.type({
+    facebook: maybe(t.boolean),
+    email: maybe(t.boolean),
+    google: maybe(t.boolean),
+    github: maybe(t.boolean),
+    phone: maybe(t.boolean),
+    twitter: maybe(t.boolean),
+});
+
+const auth = t.type({
+    provider,
+});
+
 const storage = t.type({
     enablePublic: t.boolean,
     enableUnlisted: t.boolean,
 });
 
 const firebase = t.type({
+    auth,
     storage: maybe(storage),
 });
 
 export const webConfig = t.type({
     api: maybe(api),
-    firebase: maybe(firebase),
+    firebase,
 });
 
 export type WebConfig = t.TypeOf<typeof webConfig>;
--- src/pageComponents/SignInCore.tsx
+++ src/pageComponents/SignInCore.tsx
@@ -7,11 +7,35 @@ import firebase from 'firebase/app';
 import * as firebaseui from 'firebaseui';
 import { getAuth } from '../utils/firebaseHelpers';
 import ConfigContext from '../contexts/ConfigContext';
+import { Config } from '../config';
+
+const toSignInOptions = (provider: Config['web']['firebase']['auth']['provider']) => {
+    const signInOptions: firebaseui.auth.Config['signInOptions'] = [];
+    if (provider.email === true) {
+        signInOptions.push(firebase.auth.EmailAuthProvider.PROVIDER_ID);
+    }
+    if (provider.facebook === true) {
+        signInOptions.push(firebase.auth.FacebookAuthProvider.PROVIDER_ID);
+    }
+    if (provider.github === true) {
+        signInOptions.push(firebase.auth.GithubAuthProvider.PROVIDER_ID);
+    }
+    if (provider.google === true) {
+        signInOptions.push(firebase.auth.GoogleAuthProvider.PROVIDER_ID);
+    }
+    if (provider.phone === true) {
+        signInOptions.push(firebase.auth.PhoneAuthProvider.PROVIDER_ID);
+    }
+    if (provider.twitter === true) {
+        signInOptions.push(firebase.auth.TwitterAuthProvider.PROVIDER_ID);
+    }
+    return signInOptions;
+};
 
 const SignInCore: React.FC = () => {
     const config = React.useContext(ConfigContext);
     const auth = getAuth(config);
-    const [isLoading, setIsLoading] = React.useState(true);
+    const [altMessage, setAltMessage] = React.useState<string | null>('loading...');
     const authContainerRef = React.useRef<HTMLDivElement | null>(null);
 
     React.useEffect(() => {
@@ -19,7 +43,15 @@ const SignInCore: React.FC = () => {
             return;
         }
 
-        const config: firebaseui.auth.Config = {
+        const signInOptions = toSignInOptions(config.web.firebase.auth.provider);
+        if (signInOptions.length === 0) {
+            setAltMessage(
+                'エラー: config 内の firebase.auth.provider でログインプロバイダが1つも指定されていないため、ログインできません。この問題を解決するには、サーバー管理者に問い合わせてください。'
+            );
+            return;
+        }
+
+        const firebaseAuthConfig: firebaseui.auth.Config = {
             callbacks: {
                 signInSuccessWithAuthResult: function (authResult, redirectUrl) {
                     // User successfully signed in.
@@ -30,24 +62,24 @@ const SignInCore: React.FC = () => {
                 uiShown: function () {
                     // The widget is rendered.
                     // Hide the loader.
-                    setIsLoading(false);
+                    setAltMessage(null);
                 },
             },
             signInSuccessUrl: '/',
-            signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+            signInOptions,
             // TODO: Terms of service url
             //tosUrl: '<your-tos-url>',
             // TODO: Privacy policy url.
             //privacyPolicyUrl: '<your-privacy-policy-url>'
         };
         const authUI = firebaseui.auth.AuthUI.getInstance() ?? new firebaseui.auth.AuthUI(auth);
-        authUI.start(authContainerRef.current, config);
-    }, [auth, authContainerRef]);
+        authUI.start(authContainerRef.current, firebaseAuthConfig);
+    }, [auth, authContainerRef, config.web.firebase.auth.provider]);
 
     return (
         <div>
             <div ref={authContainerRef} />
-            <span>{isLoading ? 'loading...' : null}</span>
+            <span>{altMessage}</span>
         </div>
     );
 };
