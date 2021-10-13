43587cdb13a9072bb06954e14f52c8b9f88fa011
kizahasi <rasis.aneki@gmail.com>
2021-10-13 19:37:15 +0900

    fix: fix `run gen`

--- run-buildSchemaSync.ts
+++ run-buildSchemaSync.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { buildSchemaSync, noAuthCheck } from './src/buildSchema';
-import registerEnumTypes from './src/graphql+mikro-orm/registerEnumTypes';
+import { registerEnumTypes } from './src/graphql+mikro-orm/registerEnumTypes';
 
 console.log('building GraphQL schema...');
 
