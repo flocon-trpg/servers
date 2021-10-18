b748efd88f4a6a48d26b0d6bc6ebbdcd3b24df73
kizahasi <rasis.aneki@gmail.com>
2021-10-18 23:47:44 +0900

    feat: 調査用ログを判別しやすくした

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -65,13 +65,13 @@ const useFirebaseUser = (): FirebaseUserState => {
     const [user, setUser] = React.useState<FirebaseUserState>(loading);
     React.useEffect(() => {
         if (auth == null) {
-            console.info('auth changed (null)');
+            console.info('[調査用ログ] auth changed (null)');
             setUser(authNotFound);
             return;
         }
-        console.info('authChange(non-null)');
+        console.info('[調査用ログ] authChange(non-null)');
         const unsubscribe = auth.onIdTokenChanged(user => {
-            console.info('onIdTokenChanged');
+            console.info('[調査用ログ] onIdTokenChanged');
             setUser(user == null ? notSignIn : user);
         });
         return () => {
