f7593d82375f5f6ef0bbc44d37a32e1f6d3ebe5b
kizahasi <rasis.aneki@gmail.com>
2021-10-24 22:50:00 +0900

    test: improve console message

--- test/console.test.ts
+++ test/console.test.ts
@@ -3,9 +3,9 @@ import { createConsoleClass, exec } from '../src';
 test.each(['log', 'info', 'warn', 'error'])('console', methodName => {
     const actual = exec(
         `
-console.${methodName}('JEST-CONSOLE-MESSAGE: If you see this message, even if written by console.warn or console.error, it does not mean an error');
+console.${methodName}('UNITTEST-CONSOLE-MESSAGE: If you see this message, even if written in console.warn or console.error, it does not mean an error');
         `,
-        { console: createConsoleClass('[JEST-CONSOLE-HEADER]') }
+        { console: createConsoleClass('[UNITTEST-CONSOLE-HEADER]') }
     );
     expect(actual.result).toBeUndefined();
 });
