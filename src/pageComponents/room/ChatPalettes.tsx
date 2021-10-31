67b6c277ea687f7f5561d98fd5945ce6dbe9c206
kizahasi <rasis.aneki@gmail.com>
2021-10-31 20:34:42 +0900

    fix: hide resize in ChatPalette editor

--- src/pageComponents/room/ChatPalettes.tsx
+++ src/pageComponents/room/ChatPalettes.tsx
@@ -69,7 +69,8 @@ const ChatPaletteList: React.FC<ChatPaletteListProps> = ({
     if (isEditMode) {
         return (
             <ChatPaletteTomlInput
-                style={{ minHeight: 'calc(100% - 32px)', resize: 'none' }}
+                style={{ minHeight: 'calc(100% - 32px)' }}
+                disableResize
                 size='small'
                 bufferDuration='default'
                 value={bufferedChatPaletteToml ?? ''}
