bacd82115de9024ff8ef776020946b31c6b48418
kizahasi <rasis.aneki@gmail.com>
2021-11-06 03:46:18 +0900

    test(web-server): add jest.config.json

---
+++ packages/web-server/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+
+    // これがないとテストした際に@testing-library/reactなどが正常に動かない
+    // https://zenn.dev/garypippi/articles/c79cb002e001681a73cd
+    globals: {
+        'ts-jest': {
+            tsconfig: './tsconfig.jest.json',
+        },
+    },
+};
