66966572e12cf6b8edace7606d2b9beb52196b6a
kizahasi <rasis.aneki@gmail.com>
2021-11-06 01:36:12 +0900

    fix: fix web-server

---
+++ packages/web-server/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'prettier',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+    rules: {
+        '@typescript-eslint/member-delimiter-style': ['error'],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        '@typescript-eslint/no-namespace': 'off',
+        'space-before-blocks': 'warn',
+        'react/prop-types': 'off',
+    },
+};
