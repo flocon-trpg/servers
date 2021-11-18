34b071fb11e4bd40c0fdaeac5959dc7b5c22dcff
kizahasi <rasis.aneki@gmail.com>
2021-11-19 00:09:19 +0900

    test(cache): disconnect redis on finish

--- packages/cache/test/index.test.ts
+++ packages/cache/test/index.test.ts
@@ -14,14 +14,14 @@ if (skipRedis) {
     console.log('DO Redis tests because `TEST_SKIP_REDIS` is not true');
 }
 
-const createEach = (): Cache[] => {
+const createEach = (redis: Redis.Redis): Cache[] => {
     const result: Cache[] = [];
     result.push(createNodeCache({}));
     if (!skipRedis) {
         result.push(
             createRedisCache({
                 keyPrefix: Math.random().toString(),
-                redis: new Redis(),
+                redis,
             })
         );
     }
@@ -29,36 +29,42 @@ const createEach = (): Cache[] => {
 };
 
 describe('cache', () => {
-    it.each(createEach())('tests get string value (not exists)', async cache => {
+    const redis = new Redis();
+
+    afterAll(() => {
+        redis.disconnect();
+    });
+
+    it.each(createEach(redis))('tests get string value (not exists)', async cache => {
         const getResult = await cache.getAsString('KEY');
         expect(getResult).toBeNull();
     });
 
-    it.each(createEach())('tests set and get string value (exists)', async cache => {
+    it.each(createEach(redis))('tests set and get string value (exists)', async cache => {
         const setResult = await cache.set('KEY', 'VALUE');
         expect(setResult).toBe(true);
         const getResult = await cache.getAsString('KEY');
         expect(getResult).toBe('VALUE');
     });
 
-    it.each(createEach())('tests get number value (not exists)', async cache => {
+    it.each(createEach(redis))('tests get number value (not exists)', async cache => {
         const getResult = await cache.getAsNumber('KEY');
         expect(getResult).toBeNull();
     });
 
-    it.each(createEach())('tests set and get number value (exists)', async cache => {
+    it.each(createEach(redis))('tests set and get number value (exists)', async cache => {
         const setResult = await cache.set('KEY', 17);
         expect(setResult).toBe(true);
         const getResult = await cache.getAsNumber('KEY');
         expect(getResult).toBe(17);
     });
 
-    it.each(createEach())('tests del (not exists)', async cache => {
+    it.each(createEach(redis))('tests del (not exists)', async cache => {
         const delResult = await cache.del('KEY');
         expect(delResult).toBe(false);
     });
 
-    it.each(createEach())('tests del (exists)', async cache => {
+    it.each(createEach(redis))('tests del (exists)', async cache => {
         await cache.set('KEY', 'VALUE');
         const delResult = await cache.del('KEY');
         expect(delResult).toBe(true);
@@ -66,14 +72,14 @@ describe('cache', () => {
         expect(getResult).toBeNull();
     });
 
-    it.each(createEach())('tests incr (not exists)', async cache => {
+    it.each(createEach(redis))('tests incr (not exists)', async cache => {
         const incrResult = await cache.incrby('KEY', 2);
         expect(incrResult).toBe(2);
         const getResult = await cache.getAsNumber('KEY');
         expect(getResult).toBe(2);
     });
 
-    it.each(createEach())('tests incr (exists)', async cache => {
+    it.each(createEach(redis))('tests incr (exists)', async cache => {
         await cache.set('KEY', 17);
         const incrResult = await cache.incrby('KEY', 2);
         expect(incrResult).toBe(19);
@@ -81,14 +87,14 @@ describe('cache', () => {
         expect(getResult).toBe(19);
     });
 
-    it.each(createEach())('tests decr (not exists)', async cache => {
+    it.each(createEach(redis))('tests decr (not exists)', async cache => {
         const decrResult = await cache.decrby('KEY', 2);
         expect(decrResult).toBe(-2);
         const getResult = await cache.getAsNumber('KEY');
         expect(getResult).toBe(-2);
     });
 
-    it.each(createEach())('tests decr (exists)', async cache => {
+    it.each(createEach(redis))('tests decr (exists)', async cache => {
         await cache.set('KEY', 17);
         const decrResult = await cache.decrby('KEY', 2);
         expect(decrResult).toBe(15);
