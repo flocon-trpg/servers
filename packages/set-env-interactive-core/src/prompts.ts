09453028a68d455e64eab4eb9c3cc817367185aa
kizahasi <rasis.aneki@gmail.com>
2021-11-21 12:51:58 +0900

    fix(set-env-interactive-core): improve firebaseConfig input

--- packages/set-env-interactive-core/src/prompts.ts
+++ packages/set-env-interactive-core/src/prompts.ts
@@ -2,49 +2,86 @@ import { prompt } from 'inquirer';
 import { en, ja, Lang } from './app';
 
 export const firebaseConfigPrompt = async (lang: Lang) => {
-    let message: string;
     switch (lang) {
         case en:
-            message = 'Enter a Firebase config file (JSON). The config file must be one line';
+            console.info('Enter values if Firebase config file. Do not include ".');
             break;
         case ja:
-            message =
-                'Firebase の設定ファイル (JSON) を入力してください. 設定ファイルは1行にして入力する必要があります';
+            console.info('Firebase構成ファイルの値を入力してください。" は含めないでください。');
             break;
     }
 
-    const name = 'name';
-    const result = await prompt<{ [name]: string }>({
-        type: 'input',
-        name,
-        message,
-    });
-    let toReturn = result[name].trim();
-    if (toReturn.includes('firebaseConfig')) {
-        switch (lang) {
-            case en:
-                console.warn(
-                    'The text might include "const firebaseConfig =". If so, fix and enter it again.'
-                );
-                break;
-            case ja:
-                console.warn(
-                    '"const firebaseConfig =" の部分が含まれてしまっている可能性があります。もしそうであれば修正して再度入力してください。'
-                );
-                break;
-        }
-    }
-    if (toReturn.endsWith(';')) {
-        toReturn = toReturn.replace(/;$/, '');
-        switch (lang) {
-            case en:
-                console.info('The last semicolon is automatically removed.');
-                break;
-            case ja:
-                console.info('末尾のセミコロンは自動的に除去されました。');
-                break;
-        }
+    // type: editor を使う方法は見送り。理由は、envfileが改行を含んだ文字をうまく扱えないようなのと、Firebaseサイトから取得できるのはJavaScriptオブジェクトだが環境変数として使えるのはJSONなのでkeyに"を付けるのをユーザーが忘れてしまう可能性があるため。
+
+    const apiKey = 'apiKey';
+    const authDomain = 'authDomain';
+    const databaseURL = 'databaseURL';
+    const projectId = 'projectId';
+    const storageBucket = 'storageBucket';
+    const messagingSenderId = 'messagingSenderId';
+    const appId = 'appId';
+    const result = await prompt<{
+        [apiKey]: string;
+        [authDomain]: string;
+        [databaseURL]: string;
+        [projectId]: string;
+        [storageBucket]: string;
+        [messagingSenderId]: string;
+        [appId]: string;
+    }>([
+        {
+            type: 'input',
+            name: apiKey,
+            message: apiKey,
+        },
+        {
+            type: 'input',
+            name: authDomain,
+            message: authDomain,
+        },
+        {
+            type: 'input',
+            name: databaseURL,
+            message: databaseURL,
+        },
+        {
+            type: 'input',
+            name: projectId,
+            message: projectId,
+        },
+        {
+            type: 'input',
+            name: storageBucket,
+            message: storageBucket,
+        },
+        {
+            type: 'input',
+            name: messagingSenderId,
+            message: messagingSenderId,
+        },
+        {
+            type: 'input',
+            name: appId,
+            message: appId,
+        },
+    ]);
+
+    switch (lang) {
+        case en:
+            console.info('(measurementId is skipped)');
+            break;
+        case ja:
+            console.info('(measurementId の入力はスキップされます)');
+            break;
     }
 
-    return toReturn;
+    return JSON.stringify({
+        [apiKey]: result[apiKey].trim(),
+        [authDomain]: result[authDomain].trim(),
+        [databaseURL]: result[databaseURL].trim(),
+        [projectId]: result[projectId].trim(),
+        [storageBucket]: result[storageBucket].trim(),
+        [messagingSenderId]: result[messagingSenderId].trim(),
+        [appId]: result[appId].trim(),
+    });
 };
