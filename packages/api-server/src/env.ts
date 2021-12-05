1ffb14d72a4b187700f9d16e38d524255e7279cb
kizahasi <rasis.aneki@gmail.com>
2021-12-06 01:05:38 +0900

    refactor(api-server): fix redundant code

--- packages/api-server/src/env.ts
+++ packages/api-server/src/env.ts
@@ -5,7 +5,7 @@ import { existsSync } from 'fs';
 export const loadDotenv = (): void => {
     // web-server(next.js)と近い仕様にさせて混乱を防ぐ狙いで、.env.localもサポートしている。
     // ちなみに、npmのdotenvのreadmeには「.envはコミットするな」「.env系のファイルは1つにまとめろ」と書かれている ( https://github.com/motdotla/dotenv/blob/27dfd3f034ce00b1daa72effbd91dd7788aced48/README.md#faq ) が、next.js、create-react-app、またおそらくnpmのdotenvの元となったRubygems版dotenvにはその規則は存在しないため、問題ないという判断をしている。
-    const dotenvFiles = ['.env.local', '.env'].flatMap(x => (x == null ? [] : [x]));
+    const dotenvFiles = ['.env.local', '.env'];
 
     dotenvFiles.forEach(dotenvFile => {
         if (existsSync(dotenvFile)) {
