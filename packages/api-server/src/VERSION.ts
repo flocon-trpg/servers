c4daa14b8fae0caca86a8790852715e4aa4843d2
kizahasi <rasis.aneki@gmail.com>
2021-12-08 01:44:54 +0900

    chore: bump up

--- packages/api-server/src/VERSION.ts
+++ packages/api-server/src/VERSION.ts
@@ -3,7 +3,7 @@ import { SemVer, beta } from '@flocon-trpg/utils';
 export const VERSION = new SemVer({
     major: 0,
     minor: 6,
-    patch: 0,
+    patch: 1,
     prerelease: {
         type: beta,
         version: 1,
