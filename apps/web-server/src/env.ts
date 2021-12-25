953e277f8e49908072204ec49e3c34ca20a25a2b
kizahasi <rasis.aneki@gmail.com>
2021-12-25 11:04:55 +0900

    feat(web-server): 匿名ログインに対応

--- apps/web-server/src/env.ts
+++ apps/web-server/src/env.ts
@@ -7,6 +7,7 @@ export const NEXT_PUBLIC_AUTH_PROVIDERS = 'NEXT_PUBLIC_AUTH_PROVIDERS';
 export const NEXT_PUBLIC_FIREBASE_STORAGE_ENABLED = 'NEXT_PUBLIC_FIREBASE_STORAGE_ENABLED';
 
 // TODO: これら以外にも対応させる。解説サイトとenv-interactiveもあわせて変更する。
+export const anonymous = 'anonymous';
 export const email = 'email';
 export const google = 'google';
 export const facebook = 'facebook';
