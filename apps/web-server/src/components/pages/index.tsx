81a11e5d3a900ff47441497a6393b9774922780e
kizahasi <rasis.aneki@gmail.com>
2022-01-10 12:23:13 +0900

    fix(web-server): トップページのバージョン情報の詳細を調整

--- apps/web-server/src/components/pages/index.tsx
+++ apps/web-server/src/components/pages/index.tsx
@@ -82,9 +82,9 @@ export const Index: React.FC = () => {
             <div className={classNames(flex, flexColumn)}>
                 {alert}
                 <Collapse ghost>
-                    <Collapse.Panel header='APIサーバーの詳細' key='version-info-detais-panel'>
+                    <Collapse.Panel header='詳細' key='version-info-detais-panel'>
                         <div className={classNames(flex, flexColumn)}>
-                            <div>{`supported API server versions: ${supportedApiServersAsString}`}</div>
+                            <div>{`このWebサーバーが対応しているAPIサーバーのバージョン範囲: ${supportedApiServersAsString}`}</div>
                             <div>
                                 ※
                                 prereleaseの比較は、Node.jsにおけるSemVerとは異なった方法を採用しています
