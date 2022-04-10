97a8440a30ab9cada8c7a66d844eaff927cd62bb
kizahasi <rasis.aneki@gmail.com>
2022-04-10 21:18:39 +0900

    feat(api-server): varchar(65535)をtextに変更し、default nullに変更

---
+++ apps/api-server/src/__migrations__/sqlite/Migration20220409152838.ts
@@ -0,0 +1,24 @@
+import { Migration } from '@mikro-orm/migrations';
+
+// mikro-ormではDEFAULT制約を変更するマイグレーションを生成できないため、手動で書いたコード
+export class Migration20220409152838 extends Migration {
+    replaceTextColumn({ tableName, columnName }: { tableName: string; columnName: string }) {
+        this.addSql(
+            `alter table \`${tableName}\` add column \`${columnName}_temp\` varchar null default null;`
+        );
+        this.addSql(`update \`${tableName}\` set \`${columnName}_temp\` = \`${columnName}\`;`);
+        this.addSql(`alter table \`${tableName}\` drop column \`${columnName}\`;`);
+        this.addSql(
+            `alter table \`${tableName}\` add column \`${columnName}\` varchar null default null;`
+        );
+        this.addSql(`update \`${tableName}\` set \`${columnName}\` = \`${columnName}_temp\`;`);
+        this.addSql(`alter table \`${tableName}\` drop column \`${columnName}_temp\`;`);
+    }
+
+    async up(): Promise<void> {
+        this.replaceTextColumn({ tableName: 'room_prv_msg', columnName: 'init_text_source' });
+        this.replaceTextColumn({ tableName: 'room_prv_msg', columnName: 'init_text' });
+        this.replaceTextColumn({ tableName: 'room_pub_msg', columnName: 'init_text_source' });
+        this.replaceTextColumn({ tableName: 'room_pub_msg', columnName: 'init_text' });
+    }
+}
