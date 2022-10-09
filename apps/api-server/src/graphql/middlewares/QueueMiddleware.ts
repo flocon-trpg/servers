38be0a451e5c14b78dc988fab021c892862d89bb
kizahasi <rasis.aneki@gmail.com>
2022-10-09 21:06:10 +0900

    fix(api-server): QueueMiddlewareにtimeoutを設定した。QueueMiddlewareによるエラーメッセージを改善。

--- apps/api-server/src/graphql/middlewares/QueueMiddleware.ts
+++ apps/api-server/src/graphql/middlewares/QueueMiddleware.ts
@@ -1,12 +1,20 @@
 import { MiddlewareFn } from 'type-graphql';
 import { ResolverContext } from '../../types';
-import { queueLimitReached } from '../../utils/promiseQueue';
-import { serverTooBusyMessage } from '../resolvers/messages';
+import { queueLimitReached, timeout } from '../../utils/promiseQueue';
+const timeoutMs = 10000;
 
+// TODO: エラーが発生したときはログを記録する。
 export const QueueMiddleware: MiddlewareFn<ResolverContext> = async ({ context }, next) => {
-    const result = await context.promiseQueue.next(() => next());
-    if (result.type === queueLimitReached) {
-        throw new Error(serverTooBusyMessage);
+    const result = await context.promiseQueue.nextWithTimeout(() => next(), timeoutMs);
+    switch (result.type) {
+        case queueLimitReached:
+            throw new Error(
+                'PromiseQueue rejected your operation. Server is too busy or there is a bug. / リクエストされた処理は拒否されました。サーバーに負荷がかかっているか、ソースコードにバグがあります。'
+            );
+        case timeout:
+            throw new Error(
+                'PromiseQueue timeout. Requested operation is too heavy or there is a bug. / リクエストされた処理がタイムアウトしました。処理が非常に重いか、ソースコードにバグがあります。'
+            );
     }
     return result.value;
 };
--- apps/api-server/src/graphql/resolvers/messages.ts
+++
@@ -1,1 +0,0 @@
-export const serverTooBusyMessage = 'server too busy. queue limit reached';
