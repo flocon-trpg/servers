ef2d56b9f90ee5a965046178029d320f2a25e28d
kizahasi <rasis.aneki@gmail.com>
2022-10-18 00:12:38 +0900

    build: api-serverのTypeScriptファイルをRollupでビルドするようにした

---
+++ apps/api-server/rollup.config.js
@@ -0,0 +1,36 @@
+const typescript = require('@rollup/plugin-typescript');
+const multiInput = require('rollup-plugin-multi-input');
+
+const external = [
+    // https://rollupjs.org/guide/en/#importing-packagejson
+    ...Object.keys(require('./package.json').dependencies),
+    // preserveModules=true かつ decorator が有効の場合、この 'tslib' がないとコードが想定通りの形で出力されない。
+    'tslib',
+];
+
+module.exports = [
+    {
+        input: [
+            'src/index.ts',
+            'src/run-*.ts',
+            'src/transport/**/*.ts',
+            'src/__migrations__/**/*.ts',
+        ],
+        output: {
+            dir: 'dist',
+            format: 'cjs',
+            sourcemap: true,
+            // pinoのtransportやmikro-ormのmigrationでJavaScriptファイルを参照しているため、これらを正常に動作させるためにtrueにしている。
+            preserveModules: true,
+        },
+        plugins: [
+            multiInput.default(),
+            typescript({
+                declaration: false,
+                declarationMap: false,
+                sourceMap: true,
+            }),
+        ],
+        external,
+    },
+];
