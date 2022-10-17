413389be7dc4f6f592739a919c7775f5ae547d06
kizahasi <rasis.aneki@gmail.com>
2022-10-18 00:12:07 +0900

    build: api-server以外のTypeScriptファイルをRollupでビルドするようにした
    - alias(tsconfig.jsonのpaths)によるimportに対応
    - すべてのmonorepoのプロジェクトに必ず tsconfig.json が存在するようになった。これにより、pnpでjestに関する型エラーが出る問題が起こらなくなる
    - テストコード等を除き、dependenciesにないパッケージをimportしている場合に警告が出るようになった

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,3 +1,7 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
     preset: 'ts-jest',
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
