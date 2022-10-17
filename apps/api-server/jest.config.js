0314dd1b3dd2b411ac0aae5ed4a84bc0f531ffc3
kizahasi <rasis.aneki@gmail.com>
2022-10-18 00:12:38 +0900

    build: api-serverのTypeScriptファイルをRollupでビルドするようにした

--- apps/api-server/jest.config.js
+++ apps/api-server/jest.config.js
@@ -1,5 +1,19 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const fs = require('fs');
+
+// require 関数は JSON に対応しているが JSON5 には対応していないため、コメントを表す // でエラーが出る。そのため、require 関数は使わず、テキストファイルとして読み込んでから // からなるコメント行を取り除いている。
+// なお、/* */ によるコメントは取り除いていないため、tsconfig.json に書いてはならない。
+const tsconfigText = fs
+    .readFileSync('./tsconfig.json', 'utf8')
+    .toString()
+    .replace(/^\s*(\/\/.*)$/gm, '');
+const tsconfig = JSON.parse(tsconfigText);
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$', // javascriptファイルのテストを回避している
+    moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+        prefix: '<rootDir>/',
+    }),
 };
