7a0abb40af6339bde4a018364b0ef6a541a98da3
kizahasi <rasis.aneki@gmail.com>
2022-10-18 23:31:20 +0900

    chore: tsconfigRootDirが二重に定義されている箇所を修正

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -4,7 +4,5 @@ module.exports = {
         project: './tsconfig.json',
         // vscodeのeslintが正常に動くようにtsconfigRootDirを設定している
         tsconfigRootDir: __dirname,
-        // vscodeのeslintが正常に動くようにtsconfigRootDirを設定している
-        tsconfigRootDir: __dirname,
     },
 };
