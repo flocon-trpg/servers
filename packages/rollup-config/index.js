f8bbd46185eb9d7129140fb54c8f417cfdcb4978
kizahasi <rasis.aneki@gmail.com>
2022-10-20 00:51:31 +0900

    build: rollup.config.jsを共通化

---
+++ packages/rollup-config/index.js
@@ -0,0 +1,55 @@
+const typescript = require('@rollup/plugin-typescript');
+
+/** 
+ * @example
+ * ```javascript
+const main = require('@flocon-trpg/rollup-config');
+// https://rollupjs.org/guide/en/#importing-packagejson
+const external = Object.keys(require('./package.json').dependencies);
+module.exports = main({ external });
+ * ```
+ */
+const main = ({ external }) => {
+    const input = 'src/index.ts';
+
+    const config = ({ dir, format }) => ({
+        input,
+        output: {
+            dir,
+            format,
+            sourcemap: false,
+            interop: 'auto',
+        },
+        external,
+        plugins: [typescript({ sourceMap: false })],
+    });
+
+    return [
+        config({ dir: 'dist/esm', format: 'esm' }),
+        config({
+            dir: 'dist/cjs',
+            format: 'cjs',
+        }),
+        {
+            input,
+            output: {
+                dir: 'dist/types',
+                format: 'esm',
+                sourcemap: true,
+            },
+            external,
+            plugins: [
+                typescript({
+                    declaration: true,
+                    declarationMap: true,
+                    emitDeclarationOnly: true,
+                    rootDir: 'src',
+                    declarationDir: 'dist/types',
+                    sourceMap: true,
+                }),
+            ],
+        },
+    ];
+};
+
+module.exports = main;
--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,44 +1,4 @@
-const typescript = require('@rollup/plugin-typescript');
-
-const input = 'src/index.ts';
-// https://rollupjs.org/guide/en/#importing-packagejson
+const config = require('@flocon-trpg/rollup-config');
 const external = Object.keys(require('./package.json').dependencies);
 
-const config = ({ dir, format }) => ({
-    input,
-    output: {
-        dir,
-        format,
-        sourcemap: false,
-        interop: 'auto',
-    },
-    external,
-    plugins: [typescript({ sourceMap: false })],
-});
-
-module.exports = [
-    config({ dir: 'dist/esm', format: 'esm' }),
-    config({
-        dir: 'dist/cjs',
-        format: 'cjs',
-    }),
-    {
-        input,
-        output: {
-            dir: 'dist/types',
-            format: 'esm',
-            sourcemap: true,
-        },
-        external,
-        plugins: [
-            typescript({
-                declaration: true,
-                declarationMap: true,
-                emitDeclarationOnly: true,
-                rootDir: 'src',
-                declarationDir: 'dist/types',
-                sourceMap: true,
-            }),
-        ],
-    },
-];
+module.exports = config({ external });
