b3f4ecfb6092704c7c0eab740909fcdda67e8ae0
kizahasi <21971954+kizahasi@users.noreply.github.com>
2023-02-18 13:52:03 +0900

    fix(web-server): HelpMessageTooltipのアイコンを、compactテーマが有効かどうかでサイズが変わるようにした。Storybookにdarkテーマを適用

--- apps/web-server/src/components/ui/HelpMessageTooltip/HelpMessageTooltip.stories.tsx
+++ apps/web-server/src/components/ui/HelpMessageTooltip/HelpMessageTooltip.stories.tsx
@@ -1,9 +1,15 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { HelpMessageTooltip, Props } from './HelpMessageTooltip';
+import { StorybookProvider } from '@/components/behaviors/StorybookProvider';
 
-const Main: React.FC<Props> = props => {
-    return <HelpMessageTooltip {...props} />;
+const Main: React.FC<Props & { compact: boolean }> = props => {
+    const { compact, ...restProps } = props;
+    return (
+        <StorybookProvider roomClientContextValue={null} compact={compact}>
+            <HelpMessageTooltip {...restProps} />
+        </StorybookProvider>
+    );
 };
 
 export default {
@@ -12,12 +18,18 @@ export default {
     args: {
         title: 'Title',
         children: 'Children',
+        compact: false,
     },
 } as ComponentMeta<typeof Main>;
 
 const Template: ComponentStory<typeof Main> = args => <Main {...args} />;
 
 export const Default = Template.bind({});
+
+export const Compact = Template.bind({});
+Compact.args = {
+    compact: true,
+};
 
 export const NoTitle = Template.bind({});
 NoTitle.args = {
--- apps/web-server/src/components/ui/HelpMessageTooltip/HelpMessageTooltip.tsx
+++ apps/web-server/src/components/ui/HelpMessageTooltip/HelpMessageTooltip.tsx
@@ -2,6 +2,7 @@ import * as Icons from '@ant-design/icons';
 import { Tooltip } from 'antd';
 import classNames from 'classnames';
 import React, { PropsWithChildren } from 'react';
+import { AntdThemeContext } from '@/contexts/AntdThemeContext';
 import { flex, flexRow, itemsCenter } from '@/styles/className';
 
 export type Props = {
@@ -12,12 +13,15 @@ export const HelpMessageTooltip: React.FC<Props> = ({
     title,
     children,
 }: PropsWithChildren<Props>) => {
+    const compact = React.useContext(AntdThemeContext).compact;
     return (
         <div className={flex} style={{ justifyContent: 'flex-start' }}>
             <div className={classNames([flex, flexRow, itemsCenter])} style={{ gap: '0 4px' }}>
                 {children}
                 <Tooltip title={title}>
-                    {title != null && <Icons.QuestionCircleTwoTone style={{ fontSize: 14 }} />}
+                    {title != null && (
+                        <Icons.QuestionCircleTwoTone style={{ fontSize: compact ? 14 : 16 }} />
+                    )}
                 </Tooltip>
             </div>
         </div>
