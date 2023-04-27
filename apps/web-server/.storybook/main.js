1b4f923a2b15496653d1d75f68de4ddf413d1ae3
kizahasi <21971954+kizahasi@users.noreply.github.com>
2023-04-28 08:54:25 +0900

    test(web-server): migrate to storybook v7

--- apps/web-server/.storybook/main.js
+++ apps/web-server/.storybook/main.js
@@ -1,58 +1,49 @@
-const path = require('path');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
-        'storybook-addon-next-router',
+        '@storybook/addon-mdx-gfm',
     ],
     // https://storybook.js.org/docs/react/configure/images-and-assets#serving-static-files-via-storybook-configuration
     staticDirs: ['../public'],
     webpackFinal: async config => {
-        // tsconfigのpathsを使うための設定
-        // https://stackoverflow.com/questions/71677948/how-to-add-typescript-paths-to-storybook
-        config.resolve.plugins = [new TsconfigPathsPlugin()];
-
-        // scssを@importするための設定
-        // https://blog.gaji.jp/2021/10/20/8350/
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: {
-                            auto: true,
-                        },
-                    },
-                },
-                // 'sass-loader'はStorybookのみで用いられている。そのためStorybookで必要なくなったら yarn remove sass-loader を実行したほうがいい。
-                'sass-loader',
-            ],
-            include: path.resolve(__dirname, '../'),
-        });
-
         // ModuleNotFoundError: Module not found: Error: Can't resolve 'stream' in '(dir)\server\node_modules\pino-abstract-transport\node_modules\split2' (おそらく pino で transport を指定しているのが原因)に対する対処
         config.resolve.fallback.stream = false;
-
         return config;
     },
-    // これがないと constructor(private readonly param: string) のように引数に修飾子が付いているコードでエラーが出る
-    // https://github.com/storybookjs/storybook/issues/13834#issuecomment-880646396
     babel: async options => {
+        // HACK: babelModeV7 を true にすると .babelrc.json ファイルなどを読み込んでくれるはずだが何故か読み込んでくれないため、仕方なくここに直書きしている。
         return {
             ...options,
-            plugins: options.plugins.filter(
-                x => !(typeof x === 'string' && x.includes('plugin-transform-classes'))
-            ),
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: {
+                            chrome: 100,
+                        },
+                    },
+                ],
+                '@babel/preset-typescript',
+                [
+                    '@babel/preset-react',
+                    {
+                        runtime: 'automatic',
+                    },
+                ],
+            ],
         };
     },
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-webpack5',
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
+    },
+    features: {
+        babelModeV7: true,
+    },
+    docs: {
+        autodocs: true,
     },
 };
--- apps/web-server/.storybook/preview.js
+++ apps/web-server/.storybook/preview.js
@@ -1,5 +1,4 @@
 import '../src/styles/css/main.scss';
-import '@storybook/addon-console';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 
 // 当初、ここで @sinonjs/fake-timers を使って Date を mock していたが、Board の Story で 画像が表示されなかったり縮尺が変わるという問題が発生したので取り除いた
