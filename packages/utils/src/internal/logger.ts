flocon-trpg/servers

commit b49c91f0d41bd39b9b0346e7c856d76e9a460497

feat(utils): support printf-style logging

packages/utils/src/internal/logger.ts
@@ -5,12 +5,42 @@ import { PinoLogLevel } from './parsePinoLogLevel';
 
 const defaultLogLevel = 'info';
 
-// pino の関数は any や可変パラメーターが多く引数のミスが起こりやすいので、型を狭めている。
-// この型では printf-style placeholder が使えないが、使う場面は今のところないので問題なし。
+// pino の関数は any や可変パラメーターが多く引数のミスが起こりやすいので、型を変更したり制限をかけている。
 interface LogFn {
-    (obj: Error | Record<string, unknown>, msg: string): void;
-    (msg: string): void;
+    (msg: string, ...args: readonly unknown[]): void;
+    (obj: Error | Record<string, unknown>, msg?: string, ...args: readonly unknown[]): void;
 }
+
+const toLogFn = (
+    logger: Logger,
+    pinoLevel: 'debug' | 'error' | 'fatal' | 'info' | 'warn' | 'silent' | 'trace',
+): LogFn => {
+    function print(msg: string, ...args: readonly unknown[]): void;
+    function print(
+        obj: Error | Record<string, unknown>,
+        msg?: string,
+        ...args: readonly unknown[]
+    ): void;
+    function print(
+        arg1: string | Error | Record<string, unknown>,
+        ...arg2: readonly unknown[]
+    ): void {
+        if (typeof arg1 === 'string') {
+            logger[pinoLevel](arg1, ...arg2);
+            return;
+        }
+        const [msg, ...args] = [...arg2];
+        if (typeof msg !== 'string') {
+            // TypeScript の型に従ってコードを書いている限り、ここには来ないはず。
+            throw new Error(
+                'When the first argument is an object, the second argument must be a string.',
+            );
+        }
+        logger[pinoLevel](arg1, msg, ...args);
+    }
+
+    return print;
+};
 
 type Type = {
     /** pino のインスタンスを get もしくは set できます。 */
@@ -48,27 +78,27 @@ export const loggerRef: Type = {
         currentLogger = value;
     },
     get debug() {
-        return this.value.debug.bind(this.value);
+        return toLogFn(this.value, 'debug');
     },
     get error() {
-        return this.value.error.bind(this.value);
+        return toLogFn(this.value, 'error');
     },
     get fatal() {
-        return this.value.fatal.bind(this.value);
+        return toLogFn(this.value, 'fatal');
     },
     get info() {
-        return this.value.info.bind(this.value);
+        return toLogFn(this.value, 'info');
     },
     infoAsNotice(msg) {
-        return this.info({ [notice]: true }, msg);
+        return this.info({ obj: { [notice]: true } }, msg);
     },
     get warn() {
-        return this.value.warn.bind(this.value);
+        return toLogFn(this.value, 'warn');
     },
     get silent() {
-        return this.value.silent.bind(this.value);
+        return toLogFn(this.value, 'silent');
     },
     get trace() {
-        return this.value.trace.bind(this.value);
+        return toLogFn(this.value, 'trace');
     },
 };

packages/default-pino-transport/src/index.ts
@@ -2,7 +2,7 @@
 import build from 'pino-abstract-transport';
 
 // これらの文字列を変更したら、@flocon-trpg/logger-base の文字列も変更すること！
-// 理由: @flocon-trpg/default-pino-transport は他から import されるのではく、pino で '@flocon-trpg/default-pino-transport' という文字列を渡すことでのみ参照されるという特殊性がある。そのためか、@flocon-trpg/logger-base から import するとビルドの際に警告が出る（実際に動くかどうかは確認していない）。不具合が起こる可能性を減らすため、import せずに @flocon-trpg/logger-base と同様の文字列を書いている。
+// 理由: @flocon-trpg/default-pino-transport は他から import されるのではく、pino で '@flocon-trpg/default-pino-transport' という文字列を渡すことでのみ参照されるという特殊性がある。そのためか、@flocon-trpg/default-pino-transport から @flocon-trpg/logger-base を import してしまうとビルドの際に警告が出る（実際に動くかどうかは確認していない）。不具合が起こる可能性を減らすため、import せずに @flocon-trpg/logger-base と同様の文字列を書いている。
 const LOG_FORMAT = 'LOG_FORMAT';
 const notice = 'notice';
 

commit 9e81e5cca8486f14f941fb121d50601c90a55fbf

fix: fix a bug some messages are not logged caused by b49c91f0d41bd39b9b0346e7c856d76e9a460497

packages/utils/src/internal/logger.ts
@@ -90,7 +90,7 @@ export const loggerRef: Type = {
         return toLogFn(this.value, 'info');
     },
     infoAsNotice(msg) {
-        return this.info({ obj: { [notice]: true } }, msg);
+        return this.info({ [notice]: true }, msg);
     },
     get warn() {
         return toLogFn(this.value, 'warn');