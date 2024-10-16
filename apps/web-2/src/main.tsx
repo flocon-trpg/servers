aca5c5ffb50d55179624852216fcfc54c6cc1ca8
kizahasi <21971954+kizahasi@users.noreply.github.com>
2024-10-17 06:17:24 +0900

    fix(web-server): Contextをmain.tsxに移動

--- apps/web-2/src/main.tsx
+++ apps/web-2/src/main.tsx
@@ -1,15 +1,22 @@
+/* eslint-disable react-refresh/only-export-components */
 import './styles/css/main.scss';
 
 import 'firebase/auth';
 import 'firebase/storage';
 
 import { enableMapSet } from 'immer';
-import { StrictMode } from 'react'
+import { PropsWithChildren, StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import { AllContextProvider } from './components/behaviors/AllContextProvider';
+import { loggerRef } from '@flocon-trpg/utils';
+import React from 'react';
+import { useSetupApp } from './hooks/useSetupApp';
+import { AntdThemeConfigProvider } from './components/behaviors/AntdThemeConfigProvider';
+import { App as AntdApp, Layout } from 'antd';
 
 enableMapSet();
 
@@ -23,13 +30,90 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const ThemedDiv: React.FC<PropsWithChildren<{ style?: React.CSSProperties }>> = ({
+    children,
+    style,
+}) => {
+    return (
+        <AntdThemeConfigProvider compact={false}>
+            <AntdApp>
+                <Layout style={{ minHeight: '100vh' }}>
+                    <Layout.Content>
+                        <div style={style}>{children}</div>
+                    </Layout.Content>
+                </Layout>
+            </AntdApp>
+        </AntdThemeConfigProvider>
+    );
+};
+
+const App = ({children}: PropsWithChildren) => {
+    const { config, authNotFoundState, urqlClient, reactQueryClient, clientId, httpUri, wsUri } =
+        useSetupApp();
+
+    React.useEffect(() => {
+        if (httpUri == null) {
+            return;
+        }
+        loggerRef.info(`GraphQL HTTP URL: ${httpUri}`);
+    }, [httpUri]);
+    React.useEffect(() => {
+        if (wsUri == null) {
+            return;
+        }
+        loggerRef.info(`GraphQL WebSocket URL: ${wsUri}`);
+    }, [wsUri]);
+    React.useEffect(() => {
+        loggerRef.info(`clientId: ${clientId}`);
+    }, [clientId]);
+
+    if (config == null) {
+        return <ThemedDiv style={{ padding: 5 }}>{'env.txt を確認しています…'}</ThemedDiv>;
+    }
+
+    if (config.isError) {
+        return (
+            <ThemedDiv
+                style={{ padding: 5 }}
+            >{`設定ファイルに問題があります: ${config.error}`}</ThemedDiv>
+        );
+    }
+
+    if (authNotFoundState) {
+        return (
+            <ThemedDiv style={{ padding: 5 }}>
+                {
+                    '予期しないエラーが発生しました: authNotFound / An unexpected error occured: authNotFound'
+                }
+            </ThemedDiv>
+        );
+    }
+    if (urqlClient == null || reactQueryClient == null) {
+        return (
+            <ThemedDiv style={{ padding: 5 }}>{'しばらくお待ち下さい… / Please wait…'}</ThemedDiv>
+        );
+    }
+    return (
+            <AllContextProvider
+                clientId={clientId}
+                urqlClient={urqlClient}
+                reactQueryClient={reactQueryClient}
+                roomClient={null}
+            >
+              {children}
+            </AllContextProvider>
+    );
+};
+
 // Render the app
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
+      <App>
       <RouterProvider router={router} />
+      </App>
     </StrictMode>,
   )
 }
