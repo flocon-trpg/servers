ae638d2ac607fc5bbf3a231f85d6e2871170c470
kizahasi <21971954+kizahasi@users.noreply.github.com>
2024-10-17 10:48:53 +0900

    build(web-server): ライセンスの生成方法をyarnのpluginからrollup-plugin-licenseに変更
    Floconの各パッケージがライセンス一覧に含まれないように `private: true` を付けた

--- apps/web-server/vite.config.ts
+++ apps/web-server/vite.config.ts
@@ -1,9 +1,11 @@
 // @ts-check
 
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import license from 'rollup-plugin-license';
+import path from 'path';
 
 export default defineConfig({
     plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
@@ -18,5 +20,26 @@ export default defineConfig({
     build: {
         // これがないとビルドされたファイルを用いて rooms/$id を開いたときに 'require is not defined' というエラーが出る（yarn dev では問題ない）。以前 Next.js を採用していたときに konva が canvas を参照しておりそこに 'const Canvas = require("canvas")' というコードがあってそこで `Error occurred prerendering page "/rooms/[id]". Read more: https://nextjs.org/docs/messages/prerender-error`、`Error: Cannot find module 'canvas'` というエラーが出ていたため、next/dynamic を使うことで回避していた過去がある。Vite の 'require is not defined' のエラーの原因もおそらく同様だと思われるため、transformMixedEsModules で解決できていると考えられる。
         commonjsOptions: { transformMixedEsModules: true },
+
+        rollupOptions: {
+            plugins: [
+                // https://github.com/vitejs/vite/discussions/7722#discussioncomment-4007436 を参考にして、npm パッケージのライセンスを出力するようにしている。
+                // CONSIDER: ただし、こうしている理由はただ単に Flocon では以前から licenses-npm-package.txt にライセンス一覧を出力していてそれを参照するページが web-server にあるからというだけである。このプラグインがなくても Vite（というよりは esbuild）にはビルドされた JavaScript ファイル内にライセンスを含める機能があるため、ただ単にライセンスを公開するという目的であればこのプラグインは不要である。
+                license({
+                    thirdParty: {
+                        output: path.resolve(
+                            import.meta.dirname,
+                            './dist/licenses-npm-package.txt',
+                        ),
+                    },
+                }) as Plugin,
+            ],
+        },
     },
+
+    esbuild: {
+        banner: '/*! licenses: ../licenses-npm-package.txt */',
+        // licenses-npm-package.txt にライセンス一覧が出力されるため、ライセンスのコメントを出力しないことで JavaScript のファイルサイズを減らしている。
+        legalComments: 'none',
+    }
 });
