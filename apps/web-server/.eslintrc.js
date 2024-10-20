7aea9e698470f0577a0757a0dc22abfbeb8e0b79
kizahasi <21971954+kizahasi@users.noreply.github.com>
2024-10-20 19:11:28 +0900

    fix: web-server 以外で eslint を v9 に、typescript-eslint monorepo を v8 に更新
    web-serverに@flocon-trpg/eslint-configを適用
    lintの警告をいくつか修正

--- apps/web-server/.eslintrc.js
+++ apps/web-server/.eslintrc.js
@@ -1,9 +1,25 @@
 module.exports = {
-    extends: ['@flocon-trpg/eslint-config'],
+    env: {
+        browser: true,
+        es6: true,
+    },
+    settings: {
+        react: { version: 'detect' },
+    },
+    extends: ['@flocon-trpg/eslint-config-v8', 'plugin:@next/next/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
     parserOptions: {
         project: './tsconfig.json',
         // vscodeのeslintが正常に動くようにtsconfigRootDirを設定している
         tsconfigRootDir: __dirname,
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
     plugins: ['@typescript-eslint', 'react', 'react-hooks'],
     rules: {
@@ -14,6 +30,8 @@ module.exports = {
                 additionalHooks: '^use((Memo|Callback)One|DeepCompareEffect)$',
             },
         ],
+        '@typescript-eslint/switch-exhaustiveness-check': 'error',
         'react/prop-types': 'off',
+        '@next/next/no-img-element': 'off',
     },
 };
--- packages/eslint-config/index.mjs
+++ packages/eslint-config/index.mjs
@@ -1,14 +1,27 @@
-module.exports = {
-    root: true,
-    ignorePatterns: ['**/*.d.ts', '.eslintrc.js'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'import'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-    ],
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier'
+import {flatConfigs}  from 'eslint-plugin-import';
+
+export default [...tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+    flatConfigs.recommended,
+  // { ignores: ['**/*.d.ts', '.eslintrc.js'] },
+  { ignores: ['**/*.d.ts'] },
+  {
+    // https://typescript-eslint.io/troubleshooting/typed-linting/performance/#eslint-plugin-import
+    rules: {
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'import/default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+    }
+  },
+  {
     rules: {
         '@typescript-eslint/member-delimiter-style': ['error'],
         '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -16,7 +29,9 @@ module.exports = {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/switch-exhaustiveness-check': 'error',
-        '@typescript-eslint/no-throw-literal': 'error',
+        '@typescript-eslint/no-unused-vars': 'off', // 誤検知が多いため off にしている
+        '@typescript-eslint/require-await': 'off',
+        "eqeqeq": ['warn', 'smart' ],
         'prefer-promise-reject-errors': 'error',
         'space-before-blocks': 'warn',
         'sort-imports': [
@@ -37,4 +52,5 @@ module.exports = {
         ],
         'no-console': 'warn',
     },
-};
+  }
+), eslintConfigPrettier];
