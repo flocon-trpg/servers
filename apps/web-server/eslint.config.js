eea23e56881f9fca7ec42dbf7d33718c7269ebfe
kizahasi <21971954+kizahasi@users.noreply.github.com>
2024-10-20 19:11:28 +0900

    fix: web-server 以外で eslint を v9 に、typescript-eslint monorepo を v8 に更新
    web-serverに@flocon-trpg/eslint-configを適用
    lintの警告をいくつか修正

--- apps/web-server/eslint.config.js
+++ apps/web-server/eslint.config.js
@@ -1,34 +1,32 @@
-import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import Config from '@flocon-trpg/eslint-config';
 
-export default tseslint.config(
-    { ignores: ['dist', '**/*.d.ts'] },
-    {
-        extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
-        files: ['**/*.{ts,tsx}'],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-            // other options...
-            parserOptions: {
-                project: ['./tsconfig.node.json', './tsconfig.app.json'],
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-
-            // TODO: Flocon の eslint パッケージと eslint のバージョンが異なるため暫定的に独立した rule を書いているが、パッケージを利用する
-            '@typescript-eslint/no-namespace': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
+export default tseslint.config({
+    extends: [...Config],
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', '**/*.d.ts'],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        // other options...
+        parserOptions: {
+            project: ['./tsconfig.node.json', './tsconfig.app.json'],
+            tsconfigRootDir: import.meta.dirname,
         },
     },
-);
+    plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+    },
+    rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+        // TODO: Flocon の eslint パッケージと eslint のバージョンが異なるため暫定的に独立した rule を書いているが、パッケージを利用する
+        '@typescript-eslint/no-namespace': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+});
