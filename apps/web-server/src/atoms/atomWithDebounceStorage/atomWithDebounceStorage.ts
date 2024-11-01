bc096da35b7217e43ff205b940fb6a01f15a8f3d
kizahasi <21971954+kizahasi@users.noreply.github.com>
2024-11-01 12:23:58 +0900

    refactor(web-server): refactor atomWithDebounceStorage

--- apps/web-server/src/atoms/atomWithDebounceStorage/atomWithDebounceStorage.ts
+++ apps/web-server/src/atoms/atomWithDebounceStorage/atomWithDebounceStorage.ts
@@ -1,12 +1,11 @@
 import { loggerRef } from '@flocon-trpg/utils';
-import { Option } from '@kizahasi/option';
 import { atom } from 'jotai';
 import { atomWithLazy, loadable } from 'jotai/utils';
 import { Subject, concatAll, debounceTime, map } from 'rxjs';
 
 /**
  * localForage を用いて自動保存する atom 。AsyncStorage を用いた atomWithStorage との違いは次のとおり。
- * - ストレージへの保存処理が debounce される。
+ * - 短時間で大量に set されても、ストレージへの保存処理は適度に debounce されて実行される。
  * - atom の get は async だが、set は async ではない。ただし、get がどこかで完了していない場合は set を実行しても無視される。
  * - atomSet の引数を利用することで、atom の set のカスタマイズが比較的容易に行える。
  */
@@ -30,27 +29,28 @@ export const atomWithDebounceStorage = <T, Args extends unknown[]>({
         )
         .subscribe();
 
-    // baseAtom の値は non-null から null に変えてはならない。もしそうしてしまうと、ensureAtom 内の getRoomConfig(roomId) を実行したときに少し古い値が読み込まれてしまうことがある。
-    const baseAtom = atom<Option<T>>(Option.none());
-    const ensureAtom = atom(get => {
-        const base = get(baseAtom);
-        if (base.isNone) {
-            return getItemFromStorage();
-        }
-        return base.value;
-    });
+    // T ではなくこのように { value: T } にしないと ({ value: T } | Promise<{ value: T }>) がどちらの型であるかを instanceof Promise を用いて簡単に判定できない。なぜならば T が Promise である可能性があるため。
+    const anAtom = atomWithLazy<{ value: T } | Promise<{ value: T }>>(() =>
+        getItemFromStorage().then(value => ({ value })),
+    );
     return atom<T | Promise<T>, Args, void>(
-        get => get(ensureAtom),
+        get => {
+            const result = get(anAtom);
+            if (result instanceof Promise) {
+                return result.then(({ value }) => value);
+            }
+            return result.value;
+        },
         (get, set, ...args: Args) => {
-            const awaitedEnsureAtom = loadable(ensureAtom);
+            const awaitedEnsureAtom = loadable(anAtom);
             const prevState = get(awaitedEnsureAtom);
             if (prevState.state !== 'hasData') {
-                // もし prevState を await すると、この atom の set の戻り値が Promise<void> になる。その場合は Promise<void> をawait する必要がありそう(深い論考や検証はしていないので、しなくてもいい可能性もある)だがそれは Flocon では不便なので、戻り値を Promise<void> ではなく void にするために何もせずに return している。当然ながらこの場合は set を実行しても何も起こらないという問題点があるが、Flocon のコンポーネントでは set が実行される前に get を useAtomValue などを用いてどこかで事前に実行しておりここには来ないと思われるのと、もしここに来ても致命的な事態にはならないと考えられるため問題ないと判断している。
+                // もし get(anAtom) をそのまま await すると、この atom の set の戻り値が Promise<void> になる。その場合は Promise<void> を await する必要がありそう(深い論考や検証はしていないので、しなくてもいい可能性もある)だがそれは Flocon では不便なので、戻り値を Promise<void> ではなく void にするのが目的で何もせずに return している。当然ながらこの場合は set を実行しても何も起こらないという問題点があるが、Flocon のコンポーネントでは set が実行される前に get を useAtomValue などを用いてどこかで事前に実行しておりここには来ないと思われるのと、もしここに来ても致命的な事態にはならないと考えられるため問題ないと判断している。
                 loggerRef.warn('set action is ignored at atomWithDebounceStorage.');
                 return;
             }
-            const newState = atomSet(prevState.data, ...args);
-            set(baseAtom, Option.some(newState));
+            const newState = atomSet(prevState.data.value, ...args);
+            set(anAtom, { value: newState });
             const saveRequest = async () => {
                 await setItemToStorage(newState);
             };
