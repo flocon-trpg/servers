f8ca11c94ebf0edee8d86b5e4be1ee8012b288dd
kizahasi <21971954+kizahasi@users.noreply.github.com>
2025-02-03 20:30:46 +0900

    fix: 前回のコミットにより生じたエラーを修正

--- apps/api-server/jest.config.cjs
+++ apps/api-server/jest.config.cjs
@@ -1,6 +1,6 @@
-import jest from 'jest';
+// もともとは cjs ではなく ts だったが、ts-node を install したのにも関わらず `Jest: 'ts-node' is required for the TypeScript configuration files. Make sure it is installed` というエラーが出たので、原因が特定されるまで cjs を使うことにした
 
-module.exports = async (): Promise<jest.Config> => {
+module.exports = async () => {
     return {
         collectCoverageFrom: ['**/*.(t|j)s'],
         coverageDirectory: '../coverage',
